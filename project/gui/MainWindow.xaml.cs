516f06e9708a5f55b3771f767b5920e0b39eafb3
etay <etay21@gmail.com>
2017-05-10 12:32:31 +0300

    gui v5

--- project/gui/MainWindow.xaml.cs
+++ project/gui/MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using LogicLayer;
+using MarketItems;
 
 namespace gui
 {
@@ -32,7 +33,16 @@ namespace gui
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
+           // string str = buyButton.Visibility();
+         //   tabControl.Visibility = System.Windows.Visibility.!(tabControl);
+            try
+            {
+                LogicLayer.AutoPilot.runPilot();
+            }
+            catch(Exception e2)
+            {
+                output.Text = e2.Message;
+            }
         }
 
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -61,6 +71,7 @@ namespace gui
             catch (Exception)
             {
                 output.Text = "invalid input";
+                return;
             }
             try
             {
@@ -95,6 +106,7 @@ namespace gui
                 catch (Exception)
                 {
                     output.Text = "invalid input";
+                    return;
                 }
                 try
                 {
@@ -120,6 +132,7 @@ namespace gui
                 catch (Exception)
                 {
                     output.Text = "invalid input";
+                    return;
                 }
                 try
                 {
@@ -136,14 +149,26 @@ namespace gui
 
         private void radioButton_Checked(object sender, RoutedEventArgs e)
         {
+            clearAll();
             userQButton.Visibility = System.Windows.Visibility.Visible;
 
+
         }
-        
-       
+        private void clearAll()
+        {
+            marketQText.Visibility = System.Windows.Visibility.Hidden;
+            buySellQText.Visibility = System.Windows.Visibility.Hidden;
+            labelId1.Visibility = System.Windows.Visibility.Hidden;
+            labeId2.Visibility = System.Windows.Visibility.Hidden;
+            MarketQButton.Visibility = System.Windows.Visibility.Hidden;
+            userQButton.Visibility = System.Windows.Visibility.Hidden;
+            BuySellQButton.Visibility = System.Windows.Visibility.Hidden;
+            cancelAllCommit.Visibility = System.Windows.Visibility.Hidden;
+        }
+
         private void marketQText_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+
         }
 
         private void textBox1_Copy_TextChanged(object sender, TextChangedEventArgs e)
@@ -153,15 +178,107 @@ namespace gui
 
         private void MarketQButton_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+            
+                int commodity = -1;
+                try
+                {
+                    commodity = int.Parse(marketQText.Text);
+                }
+                catch (Exception)
+                {
+                    output.Text = "invalid input";
+                    return;
+                }
+                try
+                {
+                    LogicLayer.MarketClientConnection mc = new LogicLayer.MarketClientConnection();
+                    MarketItems.MarketCommodityOffer response = (MarketCommodityOffer)mc.SendQueryMarketRequest(commodity);
+                    output.Text = response.ToString();
+                }
+                catch (Exception e1)
+                {
+                    output.Text = e1.Message;
+                }
+            }
 
         private void marketQRadioButton_Checked(object sender, RoutedEventArgs e)
         {
-           
+            clearAll();
+            marketQText.Visibility = System.Windows.Visibility.Visible;
+            MarketQButton.Visibility = System.Windows.Visibility.Visible;
+            labeId2.Visibility = System.Windows.Visibility.Visible;
+
+
+        }
+
+        private void buySellQRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            clearAll();
+            buySellQText.Visibility = System.Windows.Visibility.Visible;
+            BuySellQButton.Visibility = System.Windows.Visibility.Visible;
+            labelId1.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void userQButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                LogicLayer.MarketClientConnection mc = new LogicLayer.MarketClientConnection();
+                MarketItems.MarketUserData response = (MarketUserData)mc.SendQueryUserRequest();
+                output.Text = response.ToString();
+            }
+            catch (Exception e1)
+            {
+                output.Text = e1.Message;
+            }
+        }
+
+        private void BuySellQButton_Click(object sender, RoutedEventArgs e)
+        {
+            int id = -1;
+            try
+            {
+                id = int.Parse(buySellQText.Text);
+            }
+            catch (Exception)
+            {
+                output.Text = "invalid input";
+                return;
+            }
+            try
+            {
+                LogicLayer.MarketClientConnection mc = new LogicLayer.MarketClientConnection();
+                MarketItems.MarketItemQuery response = (MarketItemQuery)mc.SendQueryBuySellRequest(id);
+                output.Text = response.ToString();
+            }
+            catch (Exception e1)
+            {
+                output.Text = e1.Message;
+            }
+        }
+
+        private void cancelAllCommit_Click(object sender, RoutedEventArgs e)
+        {
             
-                marketQText.Visibility = System.Windows.Visibility.Visible;
-            
+            try
+            {
+                LogicLayer.MarketClientConnection mc = new LogicLayer.MarketClientConnection();
+                Boolean response = mc.cancelAllRequests();
+                output.Text = response.ToString();
+            }
+            catch (Exception e1)
+            {
+                output.Text = e1.Message;
+            }
+        }
+
+        private void cancelAllButton_Checked(object sender, RoutedEventArgs e)
+        {
+            clearAll();
+            cancelAllCommit.Visibility = System.Windows.Visibility.Visible;
+
         }
     }
 }
+
