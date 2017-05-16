f94062458fdabb39f2d16b579c093c8f2ec0d9f6
sagiede <sagiede@post.bgu.ac.il>
2017-05-17 02:12:51 +0300

    after merging to sagi

--- project/NunitTest/Properties/AssemblyInfo.cs
+++ project/NunitTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NUnitTestProject1")]
+[assembly: AssemblyTitle("NunitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NUnitTestProject1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("NunitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("067456c0-086c-46a8-b37f-1405717b7bfc")]
+[assembly: Guid("6bdb3ebd-639e-44e2-8d51-7e0ab7164cd7")]
 
 // Version information for an assembly consists of the following four values:
 //
